31b725075f51a11f4ea5a82aec5cabdd11577dd1
mr_destructive <gormeet711@gmail.com>
2023-01-23 21:11:13 +0530

    feat: nixpacks for railway and docker addition

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && daphne destructive_projects.asgi:application
--- destructive_projects/asgi.py
+++ destructive_projects/asgi.py
@@ -1,12 +1,13 @@
 import os
-
+import django
+django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'destructive_projects.settings')
 from django.urls import re_path
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from worldatlas import consumers, routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'destructive_projects.settings')
 
 #application = get_asgi_application()
 
